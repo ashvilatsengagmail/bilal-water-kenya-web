09c9dcda9a203926c5681a7ccc78ccc5b9b79f4c
ashvilatsengagmail <163639928+ashvilatsengagmail@users.noreply.github.com>
2025-08-08 11:24:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bilal-water-kenya-web/", 
   server: {
     host: "::",
     port: 8080,
